0812a367f9a60749749045236ef6464579312444
Lineu Pastorelli <lineualberto@gmail.com>
2020-04-28 18:37:12 -0300

    upload the readme

---
+++ README.md
@@ -0,0 +1,106 @@
+<h1 align="center">
+    <img alt="Github explorer" title="#GithubExplorer" src=".github/github-logo.svg" width="80px" />
+</h1>
+
+<h4 align="center">
+	Github explorer
+</h4>
+<p align="center">
+  <img alt="GitHub language count" src="https://img.shields.io/badge/2-languages-green">
+
+  <img alt="Repository size" src="https://img.shields.io/badge/360%2C1%20MB-repo%20size-yellowgreen">
+
+  <a href="https://www.linkedin.com/in/lineu-pastorelli-5165a7186">
+    <img alt="Made by DanielObara" src="https://img.shields.io/badge/made%20by-Lineu%20Pastorelli-green">
+  </a>
+
+  <a href="https://github.com/lineuxyz/github-explorer/commits/master">
+    <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/lineuxyz/GitHub-explorer">
+  </a>
+
+  <a href="https://github.com/lineuxyz/github-explorer/blob/master/LICENSE">
+  <img alt="License" src="https://img.shields.io/badge/license-MIT-brightgreen">
+  </a>
+    <img alt="Stargazers" src="https://img.shields.io/github/stars/lineuxyz/github-explorer?style=social">
+
+</p>
+<p align="center">
+  <a href="#-project">Project</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#🖥 Technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-how-to-use">How to use</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-how-to-contribute">How to contribute</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#license">License</a>
+</p>
+
+## 📚 Project
+
+Github explorer is a project in which you enter a repository and it will list a series of information about the repo, such as stars, issues, forks.
+
+<h1 align="center">
+    <img alt="Login-Page" title="Login-Page" src=".github/explorer.svg" width="200px" />
+</h1>
+
+
+## 🖥 Technologies
+
+This project was developed with the following technologies:
+
+- [React](https://reactjs.org)
+- [Github API](https://developer.github.com)
+
+## 🔖 Layout
+
+<p align="center">
+  <h3 align="center">Aplication dashboard</h3>
+  <img alt="GitHub language count" src=".github/dashboard.png">
+  <h3 align="center">Aplication details</h3>
+  <img alt="GitHub language count" src=".github/details.png">
+  <h3 align="center">Aplication error</h3>
+  <img alt="GitHub language count" src=".github/error.png">
+</p>
+
+## ❓ How To Use
+
+To clone and run this application, you'll need [Git](https://git-scm.com), [Node.js][nodejs] + [Yarn][yarn] installed on your computer.
+
+From your command line:
+
+### Clone the project
+```bash
+# Clone this repository
+$ git clone https://github.com/lineuxyz/github-explorer
+
+# Go into the repository
+$ cd github-explorer
+
+# Install dependencies
+$ yarn install
+
+# Run project
+$ yarn start
+```
+
+## 🤔 How to contribute
+
+- Make a fork;
+- Create a branck with your feature: `git checkout -b my-feature`;
+- Commit changes: `git commit -m 'feat: My new feature'`;
+- Make a push to your branch: `git push origin my-feature`.
+
+After merging your receipt request to done, you can delete a branch from yours.
+
+## 📜 License
+
+This project is under the MIT license. See the [LICENSE](LICENSE.md) for details.
+
+---
+
+Made with ❤️ by [Lineu Pastorelli]()
+
+[nodejs]: https://nodejs.org/
+[yarn]: https://yarnpkg.com/
+[vc]: https://code.visualstudio.com/
+[vceditconfig]: https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig
+[vceslint]: https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
+[prettier]: https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
-import api from '../../services/api'
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo-github.svg';
 
@@ -13,27 +13,33 @@ interface Repository {
   owner: {
     login: string;
     avatar_url: string;
-  }
+  };
 }
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
   const [repositories, setRepositories] = useState<Repository[]>(() => {
-    const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories');
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
 
     if (storagedRepositories) {
-      return JSON.parse(storagedRepositories)
-    } else {
-      return [];
+      return JSON.parse(storagedRepositories);
     }
+    return [];
   });
 
   useEffect(() => {
-    localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories))
-  }, [repositories])
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
-  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
     event.preventDefault();
     if (!newRepo) {
       setInputError('Digite o autor/nome do repositório');
@@ -43,43 +49,53 @@ const Dashboard: React.FC = () => {
     try {
       const response = await api.get<Repository>(`repos/${newRepo}`);
 
-      const repository = response.data
+      const repository = response.data;
 
       setRepositories([...repositories, repository]);
-      setNewRepo('')
-      setInputError('')
+      setNewRepo('');
+      setInputError('');
     } catch (err) {
       setInputError('Erro na busca por esse repositório');
     }
   }
 
-  return(
-  <>
-    <img src={logoImg} alt="Github Explorer" />
-    <Title>Explore repositórios no Github</Title>
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
 
-    <Form hasError={!!inputError} onSubmit={handleAddRepository}>
-      <input placeholder="Digite o nome do repositório" value={newRepo} onChange={(e) => setNewRepo(e.target.value)} />
-      <button type="submit"></button>
-    </Form>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
+        <button type="submit">Buscar repositório</button>
+      </Form>
 
-    { inputError && <Error>{inputError}</Error> }
+      {inputError && <Error>{inputError}</Error>}
 
-    <Repositories>
-      {repositories.map(repository => (
-        <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
-        <img src={repository.owner.avatar_url} alt={repository.owner.login} />
-        <div>
-          <strong>{repository.full_name}</strong>
-          <p>{repository.description}</p>
-        </div>
+      <Repositories>
+        {repositories.map((repository) => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-        <FiChevronRight size={20} />
-      </Link>
-      ))}
-    </Repositories>
-  </>
-  )
-}
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </Repositories>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
